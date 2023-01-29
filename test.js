adb5f7cb2a8b1915842ddd04d07512b718790509
Leslie <lesore0789@gmail.com>
2023-01-28 17:13:43 -0800

    Added tests to confirm understanding

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-// Your code here
+const Employee = require('./employee')
+
+let john = new Employee("John Wick", "Dog Lover");
+
+const boundSayName = john.sayName.bind(john);
+
+global.setTimeout(boundSayName, 2000);
+
+const boundOccupation = john.sayOccupation.bind(john);
+
+global.setTimeout(boundOccupation, 3000);
